a0b566dc78ea08e42a6a5e9afc280564ac6a059d
Chriserus <krzysborowiecki97@gmail.com>
2019-03-26 22:20:47 +0100

    Frontend language change, gitignore configuration

--- frontend/src/app/authorization-page/authorization-page.component.ts
+++ frontend/src/app/authorization-page/authorization-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { PasswordValidator } from './shared/password.validator';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {PasswordValidator} from './shared/password.validator';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-authorization-page',
@@ -10,51 +10,53 @@ import { Router } from '@angular/router';
 })
 export class AuthorizationPageComponent implements OnInit {
   ngOnInit;
-  get userName()
-  {
+
+  get userName() {
     return this.loginForm.get('userName');
   }
-  get password()
-  {
+
+  get password() {
     return this.loginForm.get('password');
   }
-  constructor(private fb : FormBuilder, private _router: Router){}
+
+  constructor(private fb: FormBuilder, private _router: Router) {
+  }
+
   loginForm = this.fb.group({
     userName: ['', Validators.required],
-    password : ['', Validators.required]
+    password: ['', Validators.required]
   });
 
   registrationForm = this.fb.group({
-    newEmail : ['', Validators.required],
-    newUserName : ['', [Validators.required, Validators.minLength(4)]],
-    newPassword : ['', [Validators.required, Validators.minLength(8)]],
-    newConfirmPassword : ['', Validators.required]
+    newEmail: ['', Validators.required],
+    newUserName: ['', [Validators.required, Validators.minLength(4)]],
+    newPassword: ['', [Validators.required, Validators.minLength(8)]],
+    newConfirmPassword: ['', Validators.required]
   }, {validator: PasswordValidator});
-  get newUserName()
-  {
+
+  get newUserName() {
     return this.registrationForm.get('newUserName');
   }
-  get newEmail()
-  {
+
+  get newEmail() {
     return this.registrationForm.get('newEmail');
   }
-  get newPassword()
-  {
+
+  get newPassword() {
     return this.registrationForm.get('newPassword');
   }
-  get newConfirmPassword()
-  {
+
+  get newConfirmPassword() {
     return this.registrationForm.get('newConfirmPassword');
   }
 
-  onSubmit()
-  {
-    if(this.loginForm.get('userName').value === "admin" && this.loginForm.get('password').value === "admin" )
-    {
-      alert("Zalogowano do panelu admina");
+  onSubmit() {
+    if (this.loginForm.get('userName').value === "admin" && this.loginForm.get('password').value === "admin") {
+      alert("Logged in to admin panel");
       console.log(this.loginForm);
       this._router.navigateByUrl('dashboard')
+    } else {
+      alert("Not implemented yet");
     }
-    else{ alert ("No tego jest nie zrobiłem XD");}
   }
 }
